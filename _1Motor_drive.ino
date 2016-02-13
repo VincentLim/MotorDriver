dd46db29128e5b96431ca4ef63abcb6027e95533
Vincent Limorté <vincent.limorte@gmail.com>
2016-02-13 19:32:54 +0100

    Include Motor lib

--- _1Motor_drive.ino
+++ _1Motor_drive.ino
@@ -1,48 +1,38 @@
-
+#include <MotorDriver.h>
 
 #define EN_A 5
 #define IN_A_1 6
 #define IN_A_2 7
 #define LED 13
 
+MotorDriver motor1(EN_A, IN_A_1, IN_A_2);
+
 void setup() {
-  pinMode(IN_A_1, OUTPUT);
-  pinMode(IN_A_2, OUTPUT);
-  pinMode(EN_A, OUTPUT);
+  
   pinMode(LED, OUTPUT);
-  analogWrite(EN_A, 0);
-  digitalWrite(IN_A_1, LOW);
-  digitalWrite(IN_A_2, LOW);
+  
 }
 
 void loop() {
   // Spin 5sec Clockwise
   digitalWrite(LED, HIGH);
-  analogWrite(EN_A, 255);
-  digitalWrite(IN_A_1, HIGH);
-  digitalWrite(IN_A_2, LOW);
+  motor1.run(255, 1);
 
 delay (5000);
 // STOP 1sec
   digitalWrite(LED, LOW);
-  analogWrite(EN_A, 0);
-  digitalWrite(IN_A_1, LOW);
-  digitalWrite(IN_A_2, LOW);
+ motor1.brake();
 
   delay(1000);
 
   // Spin 5sec counter Clockwise
   digitalWrite(LED, HIGH);
-  analogWrite(EN_A, 255);
-  digitalWrite(IN_A_1, LOW);
-  digitalWrite(IN_A_2, HIGH);
+  motor1.run(255, -1);
 
 delay (5000);
 // STOP 1sec
 digitalWrite(LED, LOW);
-  analogWrite(EN_A, 0);
-  digitalWrite(IN_A_1, LOW);
-  digitalWrite(IN_A_2, LOW);
+  motor1.brake();
 
   delay(1000);
 }
