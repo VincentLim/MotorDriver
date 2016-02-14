3966c8210a9691b484d28476df7363e0d1310669
VincentLim <vincent.limorte@gmail.com>
2016-02-14 01:28:37 +0100

    Timer read test

--- _1Motor_drive.ino
+++ _1Motor_drive.ino
@@ -1,16 +1,22 @@
+#include <TimerThree.h>
+
 #include <MotorDriver.h>
 
 #define EN_A 5
 #define IN_A_1 6
 #define IN_A_2 7
+#define SENS_A A0
 #define LED 13
 
-MotorDriver motor1(EN_A, IN_A_1, IN_A_2);
+MotorDriver motor1(EN_A, IN_A_1, IN_A_2, SENS_A);
 
 void setup() {
   
   pinMode(LED, OUTPUT);
-  
+  // 200 ms timer
+  Serial.begin(38400);
+  Timer3.initialize(200000);
+  Timer3.attachInterrupt(readValue);
 }
 
 void loop() {
@@ -35,6 +41,11 @@ digitalWrite(LED, LOW);
   motor1.brake();
 
   delay(1000);
+}
+
+void readValue(){
+  int readValue= motor1.sense();
+  Serial.println(readValue);
 }
 
 
